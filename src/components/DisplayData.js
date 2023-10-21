3a717287830bdf96f9d47f91dd78abb3724e8131
Gorgees Odisho <gorgees041@gmail.com>
2023-10-22 02:51:10 +0300

    DONE

---
+++ src/components/DisplayData.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayData = ({ firstName, lastName }) => {
+  return <div>{firstName + " " + lastName}</div>;
+};
+
+export default DisplayData;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import DisplayData from "./DisplayData";
 
-function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [submitData, setSubmitData] = useState([]);
+  const [errors, setErrors] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +15,43 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (firstName.length > 0) {
+      const formData = {
+        firstName: firstName,
+        lastName: lastName,
+      };
+      const storeData = [...submitData, formData];
+      setSubmitData(storeData);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    } else {
+      setErrors(["First name is required!"]);
+    }
+  }
+  console.log(errors);
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {errors.map((error, i) =>
+        error.length > 0 ? <p key={i}>{error}</p> : null
+      )}
+      {submitData.map((data, i) => {
+        return (
+          <DisplayData
+            key={i}
+            firstName={data.firstName}
+            lastName={data.lastName}
+          />
+        );
+      })}
+    </div>
   );
 }
 
